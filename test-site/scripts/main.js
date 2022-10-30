398a13d7af395e516c6329fbb0667e086f8277ef
val3602 <116677210+val3602@users.noreply.github.com>
2022-10-30 21:47:48 +0200

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,23 @@
+
+
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+} 
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'How are You, ' + myName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
